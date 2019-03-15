d493cd99244450bae6240575b8979a104285875e
jonsadev <jonsalazar99@hotmail.com>
2019-03-14 19:41:14 -0700

    web-ui: Filling a list of public address from trezor

--- web-ui/src/app/components/excalibur-connect/excalibur-connect.component.ts
+++ web-ui/src/app/components/excalibur-connect/excalibur-connect.component.ts
@@ -8,21 +8,47 @@ import TrezorConnect from 'trezor-connect';
 })
 export class ExcaliburConnectComponent implements OnInit {
 
-  trezor: TrezorConnect;
+  addresses: Array<Object>;
+  selectedFee = 'normal';
 
   constructor() { }
 
   ngOnInit() {
+    this.addresses = Array<Object>();
     TrezorConnect.manifest({
       email: 'developer@xyz.com',
       appUrl: 'http://your.application.com'
     });
-    this.getAddress();
   }
 
-  async getAddress() {
-    console.log('getAddress');
-    const result = await TrezorConnect.getAddress({path: "m/44'/0'/0'/0/1", coin: "bitcoin", showOnTrezor: false});
-    console.log(result);
+  async getTrezorAddress(path: string) {
+    const result = await TrezorConnect.getAddress({path: path, coin: 'bitcoin', showOnTrezor: false});
+    // const result = await TrezorConnect.getAddress({path: "m/44'/5'/0'/0/1", coin: "DASH", showOnTrezor: false});
+    return result;
+  }
+
+
+  getAddress() {
+    const path = 'm/44\'/0\'/0\'/0/' + this.addresses.length;
+    this.getTrezorAddress(path).then((result) => {
+      this.addresses.push({
+        hexAddress: result.payload.address,
+        pathAddress: result.payload.serializedPath
+      });
+    });
+  }
+
+  getFeeAmount(feeType): number {
+    if (feeType === 'low') {
+      return 100;
+    }
+    if (feeType === 'normal') {
+      return 500;
+    }
+    if (feeType === 'high') {
+      return 1000;
+    }
+
+    return 0;
   }
 }
